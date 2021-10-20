3fd6c555480f0bd6b4017dbbe0caf27f2ab66108
sheilalalala112 <xuanlanxu@hotmail.com>
2021-10-20 11:07:16 -0400

    updated datatable

--- app/data/coreprojectdb.sql
+++ app/data/coreprojectdb.sql
@@ -1,12 +1,12 @@
 use coreproject;
-show create table refere
+show create table referee
 
 DROP TABLE IF EXISTS referee;
 CREATE TABLE `referee` (
    `refereeid` INT PRIMARY KEY AUTO_INCREMENT,
    `firstname` varchar(15) NOT NULL,
    `lastname` varchar(15) NOT NULL,
-   `grade` varchar(1) NOT NULL,
+   `grade` int NOT NULL,
    `age` int NOT NULL,
    `rating` int NOT NULL
  );
@@ -23,12 +23,24 @@ CREATE TABLE `referee` (
 	assid INT PRIMARY KEY AUTO_INCREMENT,
     assgameid INT,
     assrefereeid INT,
-    refereestatus VARCHAR(10),
+    refereestatus VARCHAR (18),
     FOREIGN KEY (assgameid) REFERENCES game(gameid),
     FOREIGN KEY (assrefereeid) REFERENCES referee(refereeid)
 );
 
-insert into referee values (1, 'Tanvi','Hegde', 'C', 35, 78);
-insert into referee values (2, 'Xuanlan','Xu', 'F', 29, 32);
-insert into referee values (3, 'Jonah','Hoffman', 'G', 23, 67);
-insert into referee values (4, 'Jonathan','Benishay', 'E', 78, 21);
+insert into referee values (1, 'Tanvi','Hegde', 2, 35, 78);
+insert into referee values (2, 'Xuanlan','Xu', 6, 29, 32);
+insert into referee values (3, 'Jonah','Hoffman', 3, 23, 67);
+insert into referee values (4, 'Jonathan','Benishay', 9, 78, 21);
+
+insert into game values (1,'Hodge', '2021-01-23 09:37:23');
+insert into game values (2, 'Godfrey', '2021-06-07 02:45:21');
+insert into game values (3, 'Prebys', '2021-03-05 23:11:24');
+
+insert into assignment values (1, 1, 2, 'Accepted');
+insert into assignment values (2, 2, 4, 'Assigned');
+insert into assignment values (3, 3, 3, 'Tentative');
+
+select * from game;
+select * from referee;
+select * from assignment;
