f0f048e8d592cbd3580ccba2825847a50418c7a9
Jonah Hoffman <jh97@iu.edu>
2021-10-28 22:21:16 -0400

    Create assigned.php

---
+++ app/public/api/assigned/assigned.php
@@ -0,0 +1,29 @@
+<?php
+// require 'common.php';
+require 'class/DbConnection.php';
+
+// Step 1: Get a datase connection from our helper class
+$db = DbConnection::getConnection();
+
+// Step 2: Create & run the query
+$sql = 'SELECT * FROM assignment';
+$vars = [];
+
+
+// if (isset($_GET['student'])) {
+// //   // This is an example of a parameterized query
+//   $sql = 'SELECT * FROM student WHERE studentid = ?';
+//   $vars = [ $_GET['student'] ];
+// }
+
+$stmt = $db->prepare($sql);
+$stmt->execute($vars);
+
+$assignments = $stmt->fetchAll();
+
+// Step 3: Convert to JSON
+$json = json_encode($assignments, JSON_PRETTY_PRINT);
+
+// Step 4: Output
+header('Content-Type: application/json');
+echo $json;
