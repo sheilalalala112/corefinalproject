4121464e20cc8924a7070436978fba8daa898fd4
Jonah Hoffman <jh97@iu.edu>
2021-10-28 22:57:28 -0400

    commit

--- app/public/api/assigned/assigned.php
+++ app/public/api/assigned/assigned.php
@@ -6,7 +6,11 @@ require 'class/DbConnection.php';
 $db = DbConnection::getConnection();
 
 // Step 2: Create & run the query
-$sql = 'SELECT * FROM assignment';
+$sql = 'SELECT a., a.media, a.pub_year, s.name
+FROM assignment AS a, referee AS r, game AS g
+WHERE a.assrefereeid = r.refereeid
+AND a.assgameid = b.assgameid
+AND a.refereestatus = "Assigned";';
 $vars = [];
 
 
@@ -19,10 +23,10 @@ $vars = [];
 $stmt = $db->prepare($sql);
 $stmt->execute($vars);
 
-$assignments = $stmt->fetchAll();
+$assigned = $stmt->fetchAll();
 
 // Step 3: Convert to JSON
-$json = json_encode($assignments, JSON_PRETTY_PRINT);
+$json = json_encode($assigned, JSON_PRETTY_PRINT);
 
 // Step 4: Output
 header('Content-Type: application/json');
