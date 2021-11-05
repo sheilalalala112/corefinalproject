16526f92097307aef31dc198a165613984d83504
tanvihegde <84328063+tanvihegde@users.noreply.github.com>
2021-11-05 11:16:50 -0400

    edit.delete

--- app/public/js/game.js
+++ app/public/js/game.js
@@ -2,7 +2,8 @@ const GameList = {
     data() {
       return {
         game: [],
-        gameForm: {}
+        gameForm: {},
+        selectedGame: null
       }
     },
 
@@ -19,6 +20,73 @@ const GameList = {
             .catch( (err) => {
                 console.error(err);
             })
+        },
+
+        postGame(evt) { // evt - object 
+            console.log ("Test:", this.selectedGame);
+          if (this.selectedGame) { // if it's not null/info exists, call posteditbook, if the info doesn't exist, then its a new entry
+              this.postEditGame(evt);
+          } else {
+              this.postNewGame(evt);
+          }
+        },
+    
+        postEditGame(evt) {
+            this.gameForm.id = this.selectedGame.id;       
+            
+            console.log("Editing!", this.gameForm);
+            alert("Editing!");
+            fetch('api/game/update.php', {
+                method:'POST',
+                body: JSON.stringify(this.refereeForm),
+                headers: {
+                  "Content-Type": "application/json; charset=utf-8"
+                }
+              })
+              .then( response => response.json() )
+              .then( json => {
+                console.log("Returned from post:", json);
+                // TODO: test a result was returned!
+                this.game = json;
+                
+                // reset the form
+                this.handleResetEdit();
+              });
+        },
+    
+        postDeleteGame(r) {  
+            if ( !confirm("Are you sure you want to delete game " + r.firstname + "?") ) {
+                return;
+            }  
+            
+            console.log("Delete!", r);
+    
+            fetch('api/game/delete.php', {
+                method:'POST',
+                body: JSON.stringify(r),
+                headers: {
+                  "Content-Type": "application/json; charset=utf-8"
+                }
+              })
+              .then( response => response.json() )
+              .then( json => {
+                console.log("Returned from post:", json);
+                // TODO: test a result was returned!
+                this.game = json;
+                
+                // reset the form
+                this.handleResetEdit();
+              });
+          },
+
+          handleEditGame(game) {
+            this.selectedGame = game;
+            this.gameForm = Object.assign({}, this.selectedGame);
+        },
+
+        handleResetEdit() { 
+            this.selectedGame = null;
+            this.gameForm = {};
         },
 
         postNewGame(evt) {      
