c15c20d4c4c729410225bad6be83f2e4f497e63a
Jonah Hoffman <jh97@iu.edu>
2021-11-06 13:14:08 -0400

    commit

---
+++ app/public/api/referee/create.php
@@ -0,0 +1,56 @@
+<?php
+
+// if (($_SERVER['REQUEST_METHOD'] ?? '') != 'POST') {
+//     header($_SERVER["SERVER_PROTOCOL"] . " 405 Method Not Allowed");
+//     exit;
+// }
+
+// if everything is the try dies catch will do 
+try {
+    $_POST = json_decode( 
+                file_get_contents('php://input'), //get everything in the body of the request
+                true,
+                2, //Depth of 2 is the deepest it will go
+                JSON_THROW_ON_ERROR //parameter, don't fail silently
+            );
+} catch (Exception $e) { //if it dies, send a 400--> user error then exit 
+    header($_SERVER["SERVER_PROTOCOL"] . " 400 Bad Request");
+    // print_r($_POST);
+    // echo file_get_contents('php://input');
+    exit;
+}
+
+require("class/DbConnection.php");
+
+// Step 0: Validate the incoming data
+// This code doesn't do that, but should ...
+// For example, if the date is empty or bad, this insert fails.
+
+// Step 1: Get a datase connection from our helper class
+$db = DbConnection::getConnection();
+
+// Step 2: Create & run the query
+// Note the use of parameterized statements to avoid injection
+$stmt = $db->prepare( 
+  'INSERT INTO referee (firstname, lastname, grade, age, rating)
+  VALUES (?, ?, ?, ?, ?)'
+);
+// pass all these values to the query
+// info be passed later
+$stmt->execute([
+  $_POST['firstname'],
+  $_POST['lastname'],
+  $_POST['grade'],
+  $_POST['age'],
+  $_POST['rating']
+]);
+
+// Get auto-generated PK from DB
+// https://www.php.net/manual/en/pdo.lastinsertid.php
+// $pk = $db->lastInsertId();  
+
+// Step 4: Output
+// Here, instead of giving output, I'm redirecting to the SELECT API,
+// just in case the data changed by entering it
+header('HTTP/1.1 303 See Other'); //303 means so far it's succeed, for the rest of the info go to offer/?student='
+header('Location: ../referee/index.php');
---
+++ app/public/api/referee/index.php
@@ -0,0 +1,22 @@
+<?php
+// require 'common.php';
+require 'class/DbConnection.php';
+
+// Step 1: Get a datase connection from our helper class
+$db = DbConnection::getConnection();
+
+// Step 2: Create & run the query
+$sql = 'SELECT * FROM referee';
+$vars = [];
+
+$stmt = $db->prepare($sql);
+$stmt->execute($vars);
+
+$referees = $stmt->fetchAll();
+
+// Step 3: Convert to JSON
+$json = json_encode($referees, JSON_PRETTY_PRINT);
+
+// Step 4: Output
+header('Content-Type: application/json');
+echo $json;
