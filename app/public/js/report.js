f6cf4bf4753a9cdaa5dee66051d70ae1804f89b9
sheilalalala112 <xuanlanxu@hotmail.com>
2021-11-08 12:59:32 -0500

    download report

--- app/public/js/report.js
+++ app/public/js/report.js
@@ -2,8 +2,24 @@ const SomeApp = {
     data() {
       return {
         assignments: []
+        // filter: " "
       }
     },
+
+   
+    // computed: {
+    //     filteredRows() {
+    //       return this.assignments.filter(row => {
+    //         const status = assignments.refereestatus.toString().toLowerCase();
+    //         // const department = row.department.toLowerCase();
+    //         const searchTerm = this.filter.toLowerCase();
+      
+    //         return assignments.includes(searchTerm) ||
+    //           status.includes(searchTerm);
+    //       });
+    //     }
+    //   },
+
     methods: {
        
         fetchAssignmentData() {
@@ -17,6 +33,27 @@ const SomeApp = {
                 console.error(err);
             })
         }
+
+        //filter the table
+        // myFunction() {
+        //     var input, filter, table, tr, td, i, txtValue;
+        //     input = assignment.getElementById("myInput");
+        //     filter = input.value.toUpperCase();
+        //     table = assignment.getElementById("myTable");
+        //     tr = table.getElementsByTagName("tr");
+        //     for (i = 0; i < tr.length; i++) {
+        //     td = tr[i].getElementsByTagName("td")[0];
+        //     if (td) {
+        //         txtValue = td.textContent || td.innerText;
+        //         if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        //         tr[i].style.display = "";
+        //         } else {
+        //         tr[i].style.display = "none";
+        //         }
+        //     }       
+        //     }
+        // }
+
     },
     created() {
         this.fetchAssignmentData();
